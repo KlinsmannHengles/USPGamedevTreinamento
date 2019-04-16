2d38f7bb79d08f6232abdf8caf5a5bced6b81eb3
KlinsmannHengles <klinsmannhengles@gmail.com>
2019-04-16 10:58:42 -0300

    Criação do Projeto

---
+++ TreinamentoUSPGamedev/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 50f;
+
+    public float horizontalMove = 0f;
+    public float verticalMove = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    void FixedUpdate () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        verticalMove = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        Move();
+
+    }
+
+    public void Move () {
+
+        transform.Translate(horizontalMove, verticalMove, 0f);
+
+    }
+}
