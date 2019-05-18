31f9597a4b619c95b6a113872701d8d5f18b61ea
KlinsmannHengles <klinsmannhengles@gmail.com>
2019-05-18 15:58:34 -0300

    Pequenas alterações

--- TreinamentoUSPGamedev/Assets/Scripts/PlayerHealth.cs
+++ TreinamentoUSPGamedev/Assets/Scripts/PlayerHealth.cs
@@ -6,24 +6,50 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour {
 
     public int health;
-    public Image hearth1;
-    public Image hearth2;
-    public Image hearth3;
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+    public Image heart4;
+    public Image heart5;
+    public Image heart6;
+    public Image heart7;
 
 	// Use this for initialization
 	void Start () {
-		
+        heart4.enabled = false;
+        heart5.enabled = false;
+        heart6.enabled = false;
+        heart7.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (health == 2)
+		if (health == 6)
         {
-            hearth3.enabled = false;
-        } else if (health == 1) {
-            hearth2.enabled = false;
-        } else if (health == 0) {
-            hearth1.enabled = false;
+            heart7.enabled = false;
+        }
+        else if (health == 5)
+        {
+            heart6.enabled = false;
+        }
+        else if (health == 4)
+        {
+            heart5.enabled = false;
+        }
+        else if (health == 3)
+        {
+            heart4.enabled = false;
+        }
+        else if (health == 2)
+        {
+            heart3.enabled = false;
+        }
+        else if (health == 1)
+        {
+            heart2.enabled = false;
+        }
+        else if (health == 0) {
+            heart1.enabled = false;
             Destroy(gameObject, .1f);
 
         }
